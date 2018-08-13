37af4c375fa679ae57a9a966bc075116a32d99ef
Szabolcs Veer <futuremove@gmail.com>
2018-08-13 22:32:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,5 +17,6 @@ function sayHiToGrandma(string) {
   return "I can't hear you!"
   if (string === string.toUpperCase())
   return "YES INDEED!"
-  if (string)
+  if (string === "I love you, Grandma")
+  return "I love you, too."
 }
